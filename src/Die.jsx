29c8f098bbbaa67e6459ff2a568476182bb1a6c3
athulsebastiant <athulsebastiant.com>
2025-04-27 21:19:18 +0530

    fifth commit

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,11 +1,11 @@
 export default function Die(props) {
   const imageMap = {
-    1: "/src/images/die-1.png",
-    2: "/src/images/die-2.png",
-    3: "/src/images/die-3.png",
-    4: "/src/images/die-4.png",
-    5: "/src/images/die-5.png",
-    6: "/src/images/die-6.png",
+    1: "/images/die-1.png",
+    2: "/images/die-2.png",
+    3: "/images/die-3.png",
+    4: "/images/die-4.png",
+    5: "/images/die-5.png",
+    6: "/images/die-6.png",
   };
   const imageSrc = imageMap[props.value];
   const styles = {
